941de491fe64a7debe7a25ef1bc5e75b84438c2c
Kalixer <al.caceres.ag72@gmail.com>
2025-12-08 19:30:30 -0300

    [Mod => home/HomeAbout] looks a bit better, but still doesn't look professional

--- src/components/home/HomeAbout/HomeAbout.tsx
+++ src/components/home/HomeAbout/HomeAbout.tsx
@@ -1,4 +1,5 @@
 import styles from './HomeAbout.module.sass'
+import Image from 'next/image'
 
 export const HomeAbout = () => {
 
@@ -21,30 +22,53 @@ export const HomeAbout = () => {
         {
             index: 3,
             paragraphN: 4,
-            contentEng: `Currently, my goal is to grow as a more capable and versatile developer and expand my knowledge and skills until I can condifently become a Full-Stack Developer and build projects based on what I am capable of today while still being curious about new things.`,
+            contentEng: `Currently, my goal is to grow as a more capable and versatile developer and expand my knowledge and skills until I can condifently become a Full-Stack Developer and build projects based on wat I am capable of today while still being curious about new things.`,
         },
     ]
 
+    const imageSrc = 'https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=2560&h=1440&dpr=1'
+    // const imageSrc = 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
     return (
         <div className={styles.HomeAbout}>
-            <div className={styles.HomeAbout__introduction}>
-                <div className={styles.HomeAbout__introduction_titleSection}>
-                    <h1>About me</h1>
+
+            <figure className={styles.HomeAbout__ImageCont}>
+                <Image 
+                    src={imageSrc}
+                    width={500}
+                    height={500}
+                    alt="Image of a computer representing my conection to them"
+                    className={styles.HomeAbout__ImageCont_Image}
+                />
+            </figure>
+
+
+            <div className={styles.HomeAbout__content}>
+
+                <div className={styles.HomeAbout__content__legend}>
+                    <p>Curiosity <br />Growth <br />Creation</p>
                 </div>
-                <div className={styles.HomeAbout__introduction_description}>
-                    <div className={styles.HomeAbout__introduction_description_message1}>
-                        <p>Curiosity <br />Growth <br />Creation</p>
+
+
+                <div className={styles.HomeAbout__content_description}>
+
+                    <div className={styles.HomeAbout__content_description_titleSection}>
+                        <h1>About</h1>
+                        <h3>al.caceres.ag72@gmail.com</h3>
                     </div>
-                    <div className={styles.HomeAbout__introduction_description_message2}>
+
+                    <div className={styles.HomeAbout__content_description_message}>
                         {
                             content.map((element) => {
-                                const paragraph = <p className={styles.HomeAbout__introduction_description_message2_p}>{element.contentEng}</p>
+                                const paragraph = <p className={styles.HomeAbout__content_description_message_p}>{element.contentEng}</p>
 
                                 return paragraph
                             })
                         }
                     </div>
+
                 </div>
+                
             </div>
         </div>
     )
