4ba60f62a452bf32ac16bed277074a049ba93cea
Kalixer <al.caceres.ag72@gmail.com>
2025-12-12 13:26:39 -0300

    [Mod => 'HomeFastData'] I've written some info to display on the website

--- src/components/home/HomeFastData/HomeFastData.tsx
+++ src/components/home/HomeFastData/HomeFastData.tsx
@@ -1,5 +1,11 @@
 import styles from './HomeFastData.module.sass'
 
+import { FaLaptopCode } from "react-icons/fa";
+import { FaCode } from "react-icons/fa6";
+import { FaCogs } from "react-icons/fa";
+
+
+
 import { PiMusicNotesMinusBold } from "react-icons/pi";
 
 
@@ -7,19 +13,19 @@ export const HomeFastData = () => {
 
     const info = [
         {
-            icon: <PiMusicNotesMinusBold/>,
-            title: 'Título',
-            desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quis voluptas ducimus quod aspernatur nesciunt',
+            icon: <FaLaptopCode/>,
+            title: 'Years of study',
+            desc: `Since 2020 I've been studying on my own programming and how to make websites`,
         },
         {
-            icon: <PiMusicNotesMinusBold/>,
-            title: 'Título',
-            desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quis voluptas ducimus quod aspernatur nesciunt',
+            icon: <FaCode/>,
+            title: 'Technologies',
+            desc: `I'm competent with techologies like React.js, Next.js, JavaScript and Git while I still expand my knowledge`,
         },
         {
-            icon: <PiMusicNotesMinusBold/>,
-            title: 'Título',
-            desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quis voluptas ducimus quod aspernatur nesciunt',
+            icon: <FaCogs/>,
+            title: 'Projects',
+            desc: `"Better done that perfect" says the quote. I've made some projects on my own for myself and for established companies.`,
         },
     ]
 
