58368d5188fe92b69c494e71a71d7cf641b7ef98
Kalixer <al.caceres.ag72@gmail.com>
2025-12-12 12:41:11 -0300

    [Mod] Turn 'Hero' Image into a card with a white background

--- src/components/home/Welcome/Welcome.tsx
+++ src/components/home/Welcome/Welcome.tsx
@@ -5,39 +5,47 @@ import Image from "next/image";
 
 export const Welcome = () => {
     const background = 'https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1'
+
+
     return(
         <div className={styles.Welcome}>
-            <Image
-                src={background} 
-                height={100}
-                width={100}
-                alt="" 
-            />
-            <div className={styles.Welcome__Container}>
-                <div className={styles.Welcome__Container__message}>
-                    <h3>FrontEnd Dev</h3>
-                    <h1>I'm Alejandro Cáceres</h1>
-                    {/* <p className={styles.Welcome__Container__message_p}>
-                        {`I am a FrontEnd developer with skills in technologies like HTML, CSS, JS and frameworks like React.js and Next.js.
-                        Although most of my focus has been on studying, I have experience on real world projects for real companies like Solve ltda. and TopScan.
-                        {Also this website was built in React and migrated to Next.js as one of my portfolio projects.`}
-                    </p> */}
+            <div className={styles.Welcome__Card}>
+                <figure className={styles.Welcome__Card__ImageCont}>
+                    <Image
+                        src={background} 
+                        height={100}
+                        width={100}
+                        alt="" 
+                        className={styles.Welcome__Card__ImageCont_img}
+                    />
+                </figure>
 
-                    <p>I am a FrontEnd developer with skills in techologies like HTML, CSS, JS and frameworks like React.js and Next.js <br /> 
-                    Although most of my focus has been on studying, I have experience on real world projects for real companies like Solve ltda. and TopScan. <br /> 
-                    Also this website was built in React and migrated to Nexts.js as one of my portfolio projects
-                    </p>
-                </div>
-                <div className={styles.Welcome__Container__Inputs}>
-                    <button type='button' className={styles.Welcome__Container__Inputs__button}>
-                        <p>CV</p>
-                        <HiOutlineArrowNarrowRight className={styles.Welcome__Container__Inputs__button_icon}/>
-                    </button>
-                    <button type='button' className={styles.Welcome__Container__Inputs__button}>
-                        <p>Portfolio</p>
-                        <HiOutlineArrowNarrowRight className={styles.Welcome__Container__Inputs__button_icon}/>
-                    </button>
-
+                <div className={styles.Welcome__Container}>
+                    <div className={styles.Welcome__Container__message}>
+                        <h3>FrontEnd Dev</h3>
+                        <h1>I'm Alejandro Cáceres</h1>
+                        {/* <p className={styles.Welcome__Container__message_p}>
+                            {`I am a FrontEnd developer with skills in technologies like HTML, CSS, JS and frameworks like React.js and Next.js.
+                            Although most of my focus has been on studying, I have experience on real world projects for real companies like Solve ltda. and TopScan.
+                            {Also this website was built in React and migrated to Next.js as one of my portfolio projects.`}
+                        </p> */}
+    
+                        <p>I am a FrontEnd developer with skills in techologies like HTML, CSS, JS and frameworks like React.js and Next.js <br /> 
+                        Although most of my focus has been on studying, I have experience on real world projects for real companies like Solve ltda. and TopScan. <br /> 
+                        Also this website was built in React and migrated to Nexts.js as one of my portfolio projects
+                        </p>
+                    </div>
+                    <div className={styles.Welcome__Container__Inputs}>
+                        <button type='button' className={styles.Welcome__Container__Inputs__button}>
+                            <p>CV</p>
+                            <HiOutlineArrowNarrowRight className={styles.Welcome__Container__Inputs__button_icon}/>
+                        </button>
+                        <button type='button' className={styles.Welcome__Container__Inputs__button}>
+                            <p>Portfolio</p>
+                            <HiOutlineArrowNarrowRight className={styles.Welcome__Container__Inputs__button_icon}/>
+                        </button>
+                        
+                    </div>
                 </div>
             </div>
         </div>
